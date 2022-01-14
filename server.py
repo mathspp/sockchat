a52baa37b5ebb553620262584b94fdbfd6533583
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2022-01-14 17:34:41 +0000

    Receive messages from multiple clients.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import selectors
 import socket
 
 
@@ -7,19 +8,41 @@ class ChatServer:
         self._host = host
         self._port = port
         self._socket = None
+        self._read_selector = selectors.DefaultSelector()
+
+    def _accept_connection(self, sock):
+        """Callback function for when the server is ready to accept a connection."""
+        client, _ = sock.accept()
+        print("Registering client...")
+        self._read_selector.register(client, selectors.EVENT_READ, self._receive_message)
+
+    def _receive_message(self, sock):
+        """Callback function for when a client socket is ready to receive."""
+        msg = sock.recv(1024)
+        print(msg.decode("utf8"))
 
     def _init_server(self):
         """Initialises the server socket."""
+
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.bind((self._host, self._port))
         self._socket.listen()
+        # Put the socket in the selector "bag":
+        self._read_selector.register(
+            self._socket,
+            selectors.EVENT_READ,
+            self._accept_connection,
+        )
 
     def run(self):
         """Starts the server and accepts connections indefinitely."""
+
         self._init_server()
+        print("Running server...")
         while True:
-            client, _ = self._socket.accept()
-            print("Connection accepted.")
+            for key, _ in self._read_selector.select():
+                sock, callback = key.fileobj, key.data
+                callback(sock)
 
 
 if __name__ == "__main__":
