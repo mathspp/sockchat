259388a02c6d662a980778917dbed05120470099
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2022-01-18 10:24:41 +0000

    Enable clients to send AND receive messages.

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+import time
+import threading
+import selectors
 import socket
 
 
@@ -6,12 +9,19 @@ class ChatClient:
         self._host = host
         self._port = port
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._read_selector = selectors.DefaultSelector()
 
-    def connect(self):
-        self._socket.connect((self._host, self._port))
+    def _input_and_send_loop(self):
         while True:
             msg = input(">>> ")
             self._socket.send(msg.encode("utf8"))
+
+    def connect(self):
+        self._socket.connect((self._host, self._port))
+        threading.Thread(target=self._input_and_send_loop).start()
+        while True:
+            msg = self._socket.recv(1024).decode("utf8")
+            print(msg)
 
 
 if __name__ == "__main__":
